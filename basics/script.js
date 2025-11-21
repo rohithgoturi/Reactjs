2cb354611580b6d277d17c7318d9bf7a7a7c2bec
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2025-11-21 08:32:22 +0530

    import and export

--- basics/script.js
+++ basics/script.js
@@ -1,3 +1,9 @@
+import a from "./app.js";
+
 var h1 = document.createElement('h1');
 h1.innerHTML = "Hello from js 2";
 document.body.appendChild(h1);
+
+var h2 = document.createElement(h2);
+h2.innerHTML = `imported value ${a}`;
+document.body.appendChild(h2);
